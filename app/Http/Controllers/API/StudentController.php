c1ac82830cb162c161c1fbcb4d3c7801df6097dd
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2021-08-28 11:29:48 +0100

    store into the database

---
+++ app/Http/Controllers/API/StudentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    public function store(Request $req){
+       $student=new Student();
+       $student->name=$req->input('name');
+       $student->course=$req->input('course');
+       $student->email=$req->input('email');
+       $student->phone=$req->input('phone');
+       $student->save();
+
+       return response()->json([
+           'status'=>200, 
+           'message'=>'Student Added Successfully', 
+       ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::post('/add-student', [StudentController::class, 'store']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
